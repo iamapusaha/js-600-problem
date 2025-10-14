b4a0cd98ce06aa902dc247252f8d21f9292f40d2
iamapusaha <apusaha392@gmail.com>
2025-10-14 13:56:52 +0600

    added string problem

---
+++ variable-02/string-problem.js
@@ -0,0 +1,20 @@
+const name = "Apu Saha";
+console.log(name);
+
+const address = "Atrai, Naogaon";
+console.log(address);
+
+const bestFriendVarsity = "Rajshahi University";
+console.log(bestFriendVarsity);
+
+const fan = "Rashmika Mandanna";
+console.log(fan);
+
+const movieOfTheYear = "Dhumketu";
+console.log(movieOfTheYear);
+
+const hobby = "cricket";
+console.log(hobby);
+
+const color = "green";
+console.log(color);

d445ac9dca050429975c846ac22a4426d2f978f7
iamapusaha <apusaha392@gmail.com>
2025-10-14 22:18:36 +0600

    added let problem

---
+++ data-type-03/let-problem.js
@@ -0,0 +1,37 @@
+// problem no 1
+let age = 30;
+console.log(age);
+age = 32;
+console.log(age);
+
+//2
+
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney);
+
+//3
+let studyTime = 4;
+studyTime = 5;
+console.log(studyTime);
+
+//4
+let friendsCount = 3;
+friendsCount = 1;
+console.log(friendsCount);
+
+//5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice);
+
+//6
+let chairsCount = 40;
+chairsCount = chairsCount - 2;
+chairsCount = chairsCount - 6;
+console.log(chairsCount);
+
+//7
+let playTime = 3;
+playTime = 0;
+console.log(playTime);

39d860b9f31d614900cc60e507dac70d50c4d0d9
iamapusaha <apusaha392@gmail.com>
2025-10-14 13:46:33 +0600

    console prblem added

---
+++ variable-02/console-problem.js
@@ -0,0 +1,20 @@
+const age = 30;
+console.log(age);
+
+const bookToRead = 30;
+console.log(bookToRead);
+
+const wakeUpTime = 7;
+console.log(wakeUpTime);
+
+const wasteTime = 8;
+console.log(wasteTime);
+
+const borrow = 5000;
+console.log(borrow);
+
+const goals = 3;
+console.log(goals);
+
+const pillow = 3;
+console.log(pillow);

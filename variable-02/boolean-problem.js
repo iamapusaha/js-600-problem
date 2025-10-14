094d1ab9ae1aa7fe3959f6c2c7b1fde51d541834
iamapusaha <apusaha392@gmail.com>
2025-10-14 14:05:05 +0600

    added boolean problem

---
+++ variable-02/boolean-problem.js
@@ -0,0 +1,22 @@
+const isBambu = false;
+console.log(isBambu);
+
+const crushMessage = false;
+console.log(crushMessage);
+
+const celebrating = true;
+console.log(celebrating);
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy, hasMoney, isHungry);
+
+const isStudyDone = true;
+console.log(isStudyDone);
+
+const isCharged = true;
+console.log(isCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);

c775e04e97ecb4c92b958f8e81ffc67d2c995a27
iamapusaha <apusaha392@gmail.com>
2025-10-14 14:41:43 +0600

    added math problem

---
+++ variable-02/math-problem.js
@@ -0,0 +1,57 @@
+//1
+const income = 100;
+const spend = 40;
+const restMoney = income - spend;
+console.log(restMoney);
+
+//2
+const totalpencil = 10;
+const perSpendPencil = 2;
+const howManyTimeISpend = 10 / 2;
+console.log(howManyTimeISpend);
+
+//3
+const myMoney = 60;
+const perSpend = 15;
+const timeSpend = 60 / 15;
+console.log(timeSpend);
+
+//4
+const string1 = "Hello";
+const string2 = "world";
+const welcome = string1 + string2;
+console.log(welcome);
+
+//5
+const rice = 153;
+const dal = 261;
+const totalKg = rice + dal;
+console.log(totalKg);
+
+//6
+const myTotalMoney = 500;
+const perCost = 75;
+const timeICost = 500 / 75;
+const remainMoney = 500 % 75;
+console.log(perCost, remainMoney);
+
+// 7
+console.log(8 % 3);
+
+//8
+console.log(50 % 9);
+
+//9
+const firstData = "Bangla";
+const secondData = "desh";
+console.log(firstData + secondData);
+
+//10
+
+const totalRiceKg = 480;
+const perDayRice = 4;
+const totalDayRice = 480 / 4;
+const monthIdDay = 30;
+
+const totalMonthSpendRice = totalDayRice / monthIdDay;
+console.log(totalMonthSpendRice);

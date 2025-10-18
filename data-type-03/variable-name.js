2f0442da3bd3315791b8b47f1b1d2cc1f48e3006
iamapusaha <apusaha392@gmail.com>
2025-10-18 10:12:22 +0600

    added variable name problem

---
+++ data-type-03/variable-name.js
@@ -0,0 +1,30 @@
+// problem number 01
+const myFavouriteFood = "biryani";
+
+//2
+const city = "Chittagong";
+const City = "Dhaka";
+console.log(city, City);
+
+//3
+const userName = "Johan";
+const username = "Jovan";
+console.log(userName, username);
+
+//4
+const myAddress = "Atrai, Naogaon";
+
+//5
+const super = 100;
+console.log(super);
+
+//6
+const class = 5;
+console.log(class);
+
+//7
+//variable er nam kono number dehe suru kore jai na
+//variable ta amon vabe likhe jaito "secondPosition"
+const 2ndPosition = "tamim";
+console.log(2ndPosition);
+

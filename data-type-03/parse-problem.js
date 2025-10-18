87bcc31bbf5f0ce428146c8a8f612955a240f026
iamapusaha <apusaha392@gmail.com>
2025-10-18 11:58:53 +0600

    added parse problem

---
+++ data-type-03/parse-problem.js
@@ -0,0 +1,37 @@
+// problem number 01
+//yes, i can!
+const x = "20";
+const number = parseInt(x) + 10;
+console.log(number);
+
+//02
+const a = 3.14159;
+const b = parseInt(a);
+console.log(b);
+
+//03
+const boy = "premikBoy";
+const smartBoy = parseInt(boy);
+console.log(smartBoy);
+
+//04
+const c = 3.456;
+const d = 2.789;
+const f = (c + d).toFixed(2);
+console.log(f);
+
+//05
+const h = "56.78";
+const j = "12.34";
+const parseH = parseFloat(h);
+const parseJ = parseFloat(j);
+const l = parseH + parseJ;
+console.log(l);
+
+//06
+console.log((10.5678 + 5.4321).toFixed(2));
+
+//07
+console.log((42.45689754).toFixed(3));
+
+console.log(parseFloat(42.45689754));

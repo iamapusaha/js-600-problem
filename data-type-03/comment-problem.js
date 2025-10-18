4967611a1f9c6db85ded0c8651e018fa2a76c253
iamapusaha <apusaha392@gmail.com>
2025-10-18 09:55:45 +0600

    added comment related prblm

---
+++ data-type-03/comment-problem.js
@@ -0,0 +1,39 @@
+// problem no 1
+
+//bangladesh cricket team score
+const score = 350;
+
+//2
+// Stores the radius value for circle calculations
+// Should be assigned a numeric value representing length
+// Used in area or circumference formulas
+
+const radius = 80;
+
+//3
+/* a dress for puja 2025
+this dress is brand new from abbas fashon
+this dress color is red and full */
+
+const dress = "new dress";
+
+//4
+// this 100 gift me,my father for visting market
+
+const giftMoney = 100;
+//5
+//i usally study 7 hour a day.first i complete my acadmic study
+//then i start study coding related book
+//then i start philosopy book
+// then i lastly complete physics book
+
+const studyTime = 7;
+
+//6
+//i send my money to my friend 500tk only
+// and the sendmoney cost is only 5tk
+const sendMoney = 505;
+
+//7
+// i buy a headphone from online market,headphone price is 1000tk and delivery charge is 120 only.
+const pay = 1120;

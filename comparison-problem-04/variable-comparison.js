7138d93302c8e07a22a7f1e640d92ec33ddcfb2e
iamapusaha <apusaha392@gmail.com>
2025-10-20 12:09:37 +0600

    added variable comparison

---
+++ comparison-problem-04/variable-comparison.js
@@ -0,0 +1,39 @@
+//problem number 01
+const sotoVaiAge = 10;
+const myAge = 15;
+console.log(sotoVaiAge <= 15);
+
+//02
+const myMarks = 45;
+const passMarks = 33;
+console.log(myMarks >= passMarks);
+
+//03
+const pomelo = 120;
+const WoodApple = 50;
+console.log(pomelo < WoodApple);
+
+//04
+const myReadedBook = 10;
+const FriendReadedBook = 10;
+console.log(myReadedBook == FriendReadedBook);
+
+//05
+const myBiscuit = 15;
+const friendBiscuit = 20;
+console.log(myBiscuit < friendBiscuit);
+
+//06
+const realMadridGoal = 2;
+const barcelonaGoal = 3;
+console.log(realMadridGoal < barcelonaGoal);
+
+//07
+const myName = "Rahim";
+const FriendName = "Karim";
+console.log(myName === FriendName);
+
+//08
+const meIsStudy = true;
+const friendIsStudy = false;
+console.log(meIsStudy == friendIsStudy);

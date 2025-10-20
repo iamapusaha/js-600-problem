854cb2b8665a3bd8ccb54b09aca9b403a3db5fca
iamapusaha <apusaha392@gmail.com>
2025-10-20 11:24:03 +0600

    added aunty problem

---
+++ comparison-problem-04/aunty-problem.js
@@ -0,0 +1,32 @@
+//problem no 01
+const me = 7;
+const sotoVai = 5;
+
+console.log(me > sotoVai);
+
+//2
+const myClass = 45;
+const otherClass = 50;
+
+console.log(myClass > otherClass);
+
+//3
+const myResult = 75;
+const friendResult = 75;
+console.log(myResult == friendResult);
+
+//4
+const myTrget = 100;
+const myEarning = 95;
+console.log(myEarning >= myTrget);
+
+//5
+console.log(10 != 7);
+
+//6
+console.log(20 <= 15);
+
+//7
+const myFriendNumber = 97;
+const myNumber = 45;
+console.log(myFriendNumber < myNumber);

118b4267babe74c5dfaab7a0f4e432c52626b6b8
iamapusaha <apusaha392@gmail.com>
2025-10-22 21:17:59 +0600

    added discount prblm

---
+++ if-else-04/discount-problem.js
@@ -0,0 +1,67 @@
+// page no 83
+//Problem number 01
+// for 4500tk discount is 5%
+const total = 10500;
+if (total >= 6000) {
+  console.log("discount is 15%");
+} else if (total >= 3000) {
+  console.log("discount is 5%");
+}
+
+//02
+const age = 10;
+if (age < 12) {
+  console.log("meal is free for you!");
+} else if (age > 60) {
+  console.log("you have got 50% discount");
+} else {
+  console.log("you don't have any discount");
+}
+
+//03
+const balance = 1000;
+if (balance < 1000) {
+  console.log("please diposit");
+} else if (balance >= 1000 && balance <= 5000) {
+  console.log("bindas life");
+} else {
+  console.log("tui borolux tui amai biya kor");
+}
+//04
+const marks = 40;
+if (marks < 50) {
+  console.log("you are fail");
+} else if (marks >= 50 && marks < 80) {
+  console.log("you are pass");
+} else {
+  console.log("you got A+");
+}
+
+//05
+const totalPage = 499;
+if (totalPage < 100) {
+  console.log("small book");
+} else if (totalPage >= 100 && totalPage <= 500) {
+  console.log("Mid-size book");
+} else {
+  console.log("heart attack size book");
+}
+
+//06
+const temparature = 10;
+if (temparature < 0) {
+  console.log("Ice");
+} else if (temparature >= 0 && temparature <= 20) {
+  console.log("cool cool");
+} else if (temparature > 20) {
+  console.log("hot hot");
+}
+//07
+const playLevel = 15;
+if (playLevel < 10) {
+  console.log("you are novic");
+} else if (playLevel >= 10 && playLevel <= 50) {
+  console.log("You are expert");
+} else {
+  console.log("you are pro player");
+}

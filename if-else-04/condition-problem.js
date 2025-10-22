ffe6218feb362d80ff2f2c621a881f31ad3d45d0
iamapusaha <apusaha392@gmail.com>
2025-10-22 11:25:34 +0600

    solve condition problem

---
+++ if-else-04/condition-problem.js
@@ -0,0 +1,47 @@
+//Practice problem number 01
+const homeBack = 6;
+if (homeBack < 5) {
+  console.log("Maa give me some your special nasta");
+} else {
+  console.log("ei bar er moto maf kore daw maa");
+}
+
+//02
+const logIn = true;
+if (logIn) {
+  console.log("welcome");
+} else {
+  console.log("Get Lost");
+}
+
+//03
+const run = 5;
+if (run >= 5) {
+  console.log("wow! you got chocolate");
+} else {
+  console.log("you are a super man,with mota vuri!");
+}
+
+//04
+const bikeMarks = 80;
+const yourMarks = 85;
+if (yourMarks >= bikeMarks) {
+  console.log("Yes, you got a bike");
+} else {
+  console.log("sorry bor! try again later.");
+}
+//05
+const movieTime = 10;
+if (movieTime < 9) {
+  console.log("yes,watch the movie");
+} else {
+  console.log("jai basai ghum paice");
+}
+
+//06
+const temparature = 40;
+if (temparature < 30) {
+  console.log("please trun on the air condition");
+} else {
+  console.log("komble muri daw vai ghum valo hbe");
+}

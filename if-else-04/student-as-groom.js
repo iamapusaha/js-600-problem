51bc89b519f8d591f7969cb964bf29b8c4452e6c
iamapusaha <apusaha392@gmail.com>
2025-10-22 14:25:00 +0600

    added student as groom

---
+++ if-else-04/student-as-groom.js
@@ -0,0 +1,78 @@
+//practice problem number 01
+const age = 30;
+const height = 72;
+if (age > 18 && height > 60) {
+  console.log("please pull the car");
+} else {
+  console.log("please sit in the car");
+}
+
+//02
+const math = 70;
+const english = 90;
+if (math > 80 || english > 80) {
+  console.log("welcome to the university");
+} else {
+  console.log("tore biya deha debo");
+}
+
+//03
+const gpa = 5;
+const montlyIncome = 9999;
+if (gpa == 5 && montlyIncome < 10000) {
+  console.log("you are seleted for 90% scholarship");
+} else {
+  console.log("please pay you tution fee the continue here");
+}
+
+//04
+const myAge = 29;
+const jobEx = 2;
+if (myAge < 30 && jobEx > 2) {
+  console.log("you are seleted for the exam");
+} else {
+  console.log("sorry,you are not seleted for the exam");
+}
+
+//05
+const egg = 12;
+const chicken = 0;
+if (egg > 12 || chicken >= 1) {
+  console.log("go for egg korma");
+} else {
+  console.log("tui vai bread and banana kha beshi kore");
+}
+
+//06
+const temparature = 98;
+const cough = false;
+if (temparature > 100 || cough == true) {
+  console.log("feel free to go to a doctor");
+} else {
+  console.log("laap muri ghuma pagla");
+}
+
+//07
+const attendance = 80;
+const isHomeWorkDone = false;
+if (attendance > 80 && isHomeWorkDone == true) {
+  console.log("sit for the exam now");
+} else {
+  console.log("you are auto fail");
+}
+
+//08
+const loadShidding = true;
+const isPhoneCharged = false;
+if (loadShidding == true && isPhoneCharged == false) {
+  console.log("vaiya,tik moto porte bosho");
+} else {
+  console.log("play video game");
+}
+
+//09
+const shirtPrice = 1001;
+const haveCoupon = true;
+if (shirtPrice > 1000 && haveCoupon == true) {
+  console.log("yes! you are seleted for 20% discount");
+}

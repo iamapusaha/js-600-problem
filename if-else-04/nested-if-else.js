a97b47923f70b672b5ca2d5766ca6feba474df89
iamapusaha <apusaha392@gmail.com>
2025-10-22 21:58:48 +0600

    solve nested if else problem

---
+++ if-else-04/nested-if-else.js
@@ -0,0 +1,64 @@
+// page no 86
+//Practice problem no 01
+const isInvited = false;
+const pocketBalance = 200;
+if (isInvited) {
+  if (pocketBalance > 1000) {
+    console.log("celebrate birthday party with gift");
+  } else {
+    console.log("joning birthdat party without cake");
+  }
+} else {
+  console.log("bondhu ja tore friend list r dakmu na");
+}
+
+//02
+const drinkTea = true;
+const eatBiscuit = false;
+if (drinkTea) {
+  if (eatBiscuit) {
+    console.log("drink tea with biscuit");
+  } else {
+    console.log("only tea ready");
+  }
+} else {
+  console.log("bosha bosha star jalsha dakhun");
+}
+
+//03
+const isActive = true;
+const subscribtion = false;
+if (isActive) {
+  if (subscribtion) {
+    console.log("premmium user!");
+  } else {
+    console.log("you are a free user");
+  }
+} else {
+  console.log("your account is not active!");
+}
+
+//04
+const isFoodInFridge = false;
+const foodPandaWorking = false;
+if (isFoodInFridge) {
+  console.log("need to food warm first");
+} else {
+  if (foodPandaWorking) {
+    console.log("need to order food from foodpanda");
+  } else {
+    console.log("today i am roja");
+  }
+}
+//05
+const totalGuest = 1000;
+const withGift = false;
+if (totalGuest > 100) {
+  if (withGift) {
+    console.log("Let's party all night");
+  } else {
+    console.log("i will party with myself");
+  }
+} else {
+  console.log("jow baba ghor a jaw ajke party hbe na");
+}

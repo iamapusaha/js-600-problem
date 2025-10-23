b067cbe44b55848a35c6c9ca2e7d9f2a3b224d74
iamapusaha <apusaha392@gmail.com>
2025-10-23 20:46:51 +0600

    solve array length problem

---
+++ array-05/array-index.js
@@ -0,0 +1,46 @@
+// page no 96
+//Practice problem no 01
+const friends = ["rahim", "karim", "jobbar", "akash", "batas", "kalu"];
+console.log(friends[3]);
+
+//02
+const bookList = [
+  "To Kill a Mockingbird",
+  "1984",
+  "The Great Gatsby",
+  "Pride and Prejudice",
+  "Moby-Dick",
+  "War and Peace",
+  "The Catcher in the Rye",
+];
+console.log(bookList[4]);
+
+//03
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[6] = 30;
+console.log(numbers);
+
+//04
+const games = ["PUBG", "freeFire", "candy crush", "tample run"];
+games[2] = "subway surfers";
+console.log(games);
+
+//05
+const familyMembers = ["Apu saha", "addoito saha", "shilpi rani"];
+console.log(familyMembers[2]);
+
+//06
+const travelOn = [
+  "japan",
+  "Englind",
+  "USA",
+  "Africa",
+  "Australia",
+  "Finland",
+  "Indonesia",
+];
+console.log(travelOn[3]);
+
+//07
+const onTable = ["monitor", "Mobile", "book", "brash"];
+console.log(onTable[6]);

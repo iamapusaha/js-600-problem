8f7fe23c89ee42b871a08b8fb14e6692349064e3
iamapusaha <apusaha392@gmail.com>
2025-10-23 14:17:23 +0600

    solve array length problem

---
+++ array-05/array-and-length.js
@@ -0,0 +1,51 @@
+//page no 93
+//Practice problem no 01
+const number = [71, 79];
+
+//02
+const fruits = ["apple", "banana", "orange", "mango", "guava"];
+
+//03
+const vegetable = [
+  "potato",
+  "onion",
+  "begun",
+  "radish",
+  "crrot",
+  "tomato",
+  "okra",
+  "khodu",
+  "cucumber",
+  "potol",
+];
+console.log(vegetable.length);
+//04
+const movieList = [
+  "Moddhobitto",
+  "Kishor Gang",
+  "Borbaad",
+  "Daagi",
+  "Dark World",
+];
+//05
+const oddNumber = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+//06
+const colors = ["black", "white", "green", "red", "off white"];
+const colorLength = colors.length;
+console.log(colorLength);
+
+//07
+const capitalCity = [
+  "Paris",
+  "Rome",
+  "Islamabad",
+  "Seoul",
+  "Tokyo",
+  "Buenos Aires",
+  "Cape Town",
+  "Stockholm",
+  "Amsterdam",
+  "Vienna",
+];
+console.log(capitalCity.length);

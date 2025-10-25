8609bd50f3f1aef7177d1973caf285ebc6d47e0e
iamapusaha <apusaha392@gmail.com>
2025-10-25 11:21:58 +0600

    solve include indexOf problem

---
+++ array-05/include-indexof.js
@@ -0,0 +1,31 @@
+//page no 102
+//Practice problem no 01
+const fruits = ["Apple", "Banana", "Mango", "Lichi"];
+if (fruits.includes("Mango")) {
+  console.log("Amm ace");
+} else {
+  console.log("Amm nai, gacha utt");
+}
+//02
+const polapan = ["babul", "alif", "choton"];
+console.log(polapan.indexOf("babul"));
+//03
+const friendList = ["Rimon", "Rifat", "Choton"];
+const rifatIndex = friendList.indexOf("Rifat");
+console.log(rifatIndex);
+
+//04
+const city = ["Dhaka", "Chittagong", "Sylhet"];
+const isRajshahiExist = city.includes("Rajshahi");
+console.log(isRajshahiExist);
+
+//05
+const weather = ["Dighi", "Megh", "Bristy", "Borsha"];
+if (weather.includes("Bristy")) {
+  console.log("I need umberalla");
+} else {
+  console.log("No rain no pain");
+}
+//06
+const favSport = ["football", "cricket", "volleyball"];
+console.log(favSport.includes("badminton"));

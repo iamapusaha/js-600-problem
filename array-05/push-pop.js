045a632aef5a48238971c240c599d30c2b9d9e08
iamapusaha <apusaha392@gmail.com>
2025-10-25 11:01:09 +0600

    solve push pop problem

---
+++ array-05/push-pop.js
@@ -0,0 +1,29 @@
+// page no 99
+//Practice problem 01
+const numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+console.log(numbers);
+
+//02
+const friends = ["sojib", "sagor", "sakib", "shohel"];
+friends.push("sumon");
+console.log(friends);
+
+//03
+const game = ["29", "PUBG", "free fire", "stardew vally"];
+console.log(game);
+//04
+const number = [24, 36, 48, 60];
+number.unshift(12);
+console.log(number);
+
+//05
+const readedBook = [
+  "Pride and Prejudice",
+  "The Great Gatsby",
+  "A Tale of Two Cities",
+  "The Hunger Games",
+  "Jane Eyre",
+];
+readedBook.shift();
+console.log(readedBook);
